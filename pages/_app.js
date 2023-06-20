f9f47b435c21c90e627cc80e1c6d3510b25a2e1d
Shinji Esaki <shinji.esk@gmail.com>
2023-06-20 21:32:54 +0900

    add GA4

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import ReactGA from "react-ga4";
+
+// ...
+
+if (isEnvironment("production")) {
+  ReactGA.initialize("G-068C110J0L");
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Plasticity Unofficial Document</span>,
   project: {
-    link: 'https://github.com/shinjiesk/plasticity-unofficial-document',
+    link: "https://github.com/shinjiesk/plasticity-unofficial-document",
   },
-  docsRepositoryBase: 'https://github.com/shinjiesk/plasticity-unofficial-document',
+  docsRepositoryBase:
+    "https://github.com/shinjiesk/plasticity-unofficial-document",
   footer: {
-    text: ((
+    text: (
       <span>
-        {new Date().getFullYear()}{'  '}©{'  '}Shinji esk
+        {new Date().getFullYear()}
+        {"  "}©{"  "}Shinji esk
       </span>
-    ))
+    ),
   },
   darkMode: true,
   sidebar: {
     toggleButton: true,
   },
-  gitTimestamp: ({ timestamp }) => <span>Last updated on {timestamp.toLocaleDateString()}</span>,
+  gitTimestamp: ({ timestamp }) => (
+    <span>Last updated on {timestamp.toLocaleDateString()}</span>
+  ),
+};
 
-}
-
-
-export default config
+export default config;
