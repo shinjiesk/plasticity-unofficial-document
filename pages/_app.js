dfc2f8a2cbd2dad3329bedd89b8a9230238f4c2e
Shinji Esaki <shinji.esk@gmail.com>
2023-06-30 18:02:31 +0900

    add: vercel anaritycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 };
 
 export default App;
-
