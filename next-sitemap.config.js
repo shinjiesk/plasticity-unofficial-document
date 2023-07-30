2e9f5173c31b9b89e23ddbbe7f438e7ee3be0511
Shinji Esaki <shinji.esk@gmail.com>
2023-07-30 12:26:06 +0900

    edit: Sitemap process

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://plasticitydoc.vercel.app",
-  generateRobotsTxt: true, // (optional)
-  alternateRefs: ["ja"],
-  // ...other options
-};
