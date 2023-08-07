0eac566c5b75d5c082f3152ea158a007d16bfcee
Shinji Esaki <shinji.esk@gmail.com>
2023-08-07 16:47:37 +0900

    edit: Download PDF

---
+++ components/downloadPDF.jsx
@@ -0,0 +1,10 @@
+export default function DownloadPDF() {
+    return(
+        <a href="https://drive.google.com/file/d/15kxn-PaXngVzmW8hAdsZP50HEZvSklxT/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <div className="downloadButton">
+                Plasticity_1.2.4_Shortcut_Key_Sheet.pdf
+            </div>
+        </a>
+    )
+
+}
