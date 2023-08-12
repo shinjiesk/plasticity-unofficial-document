f5b0bac65b22e3ba7fa349047d7ae2a4661918cd
Shinji Esaki <shinji.esk@gmail.com>
2023-08-12 09:59:32 +0900

    update: Shortcut Keys Sheet

--- components/downloadPDF.jsx
+++ components/downloadPDF.jsx
@@ -1,10 +1,16 @@
 export default function DownloadPDF() {
-    return(
-        <a href="https://drive.google.com/file/d/15kxn-PaXngVzmW8hAdsZP50HEZvSklxT/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <div className="downloadButton">
-                Plasticity_1.2.4_Shortcut_Key_Sheet.pdf
-            </div>
-        </a>
-    )
-
+    return (
+        <>
+            <br />
+            <a
+                href="https://drive.google.com/file/d/1e1VipXfWm20W-hABjoNdqZ8d8eKI5Y79/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <div className="downloadButton">
+                    Plasticity_1.2.6_Shortcut_Key_Sheet.pdf
+                </div>
+            </a>
+        </>
+    );
 }
