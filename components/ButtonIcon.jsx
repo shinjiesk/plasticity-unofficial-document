c9c59474e2b858fda78421a59ed4153b2da2509d
Shinji Esaki <shinji.esk@gmail.com>
2023-08-19 11:43:59 +0900

    update: Outliner

--- components/ButtonIcon.jsx
+++ components/ButtonIcon.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from "next/image";
-import styles from 'styles/ButtonIcon.module.css'
+import styles from "styles/ButtonIcon.module.css";
 
-function ButtonImage({ type }) {
+function ButtonImage({ type, double = false }) {
     const imagePath = `/img/${type}.png`;
 
     return (
         <span className={styles.buttonIcon}>
-            <Image src={imagePath} alt={type} width={32} height={32} />
+            {double ? (
+                <Image src={imagePath} alt={type} width={68} height={32} />
+            ) : (
+                <Image src={imagePath} alt={type} width={32} height={32} />
+            )}
         </span>
     );
 }
