2682d3859c3f53fd80d56f6c29c1e6c4c614945e
Shinji Esaki <shinji.esk@gmail.com>
2023-08-20 13:47:25 +0900

    Add Tools video

---
+++ components/VideoPlayer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const VideoPlayer = ({ fileName }) => {
+    const videoPath = `/img/${fileName}.mp4`;
+
+    return (
+        <div>
+            <video controls >
+                <source src={videoPath} type="video/mp4" />
+                Your browser does not support video tags.
+            </video>
+        </div>
+    );
+};
+
+export default VideoPlayer;
