46c18ce6e437342b1af4041fa02b005538e4e272
Shinji Esaki <shinji.esk@gmail.com>
2023-11-22 21:04:34 +0900

    update

---
+++ components/KeyboardShortcutsTitle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "styles/KeyboardShortcutsTitle.module.css";
+
+const KeyboardShortcutsTitle = ({ children }) => {
+    return (
+        <>
+        <span className={styles.container}>{children}</span>
+        </>
+    )
+};
+
+export default KeyboardShortcutsTitle;
