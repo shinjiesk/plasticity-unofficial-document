35b14bb028b21651604ba74aec5dbf4284133b76
Shinji Esaki <shinji.esk@gmail.com>
2023-12-16 21:33:55 +0900

    Update: Shortcut key list

--- components/downloadPDF.jsx
+++ components/downloadPDF.jsx
@@ -5,12 +5,12 @@ export default function DownloadPDF() {
         <>
             <br />
             <a
-                href="https://app.box.com/s/8dnti9f978s38ihe8awk1egsu0c35dj1"
+                href="https://app.box.com/s/l21381x6kjth2cyz25qpdesn3ix4d7fv"
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 <div className="nx-text-primary-600">
-                    Plasticity_1.3_Shortcut_Key_Sheet_rev.1.pdf
+                    Plasticity_1.4_Shortcut_Key_Sheet_rev.1.pdf
                 </div>
             </a>
         </>
